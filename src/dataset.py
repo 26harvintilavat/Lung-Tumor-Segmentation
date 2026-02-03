f60b6ac38fddaa7f5f17c62bfb2f1dcfad904576
26harvintilavat <tilavatharvin@gmail.com>
2026-02-03 22:51:31 +0530

    Add dataset abstraction with spacing-aware CT loading

---
+++ src/dataset.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import numpy as np
+import pydicom
+from src.preprocessing import convert_to_hu, window_image
+
+class LungCTDataset:
+    def __init__(self, series_dir: Path):
+        self.series_dir = series_dir
+        self.slices = self._load_slices()
+
+        self.pixel_scaling = self._get_pixel_spacing()
+        self.slice_thickness = self._get_slice_thickness()
+        self.voxel_spacing = self._get_voxel_spacing()
+
+        self.volume = self._build_volume()
+
+    def _load_slices(self):
+        dicom_files = sorted(
+            self.series_dir.glob("*.dcm"),
+            key=lambda x: pydicom.dcmread(x).InstanceNumber
+        )
+        return [pydicom.dcmread(f) for f in dicom_files]
+    
+    def _get_pixel_spacing(self):
+        return tuple(map(float, self.slices[0].PixelSpacing))
+    
+    def _get_slice_thickness(self):
+        return float(self.slices[0].SliceThickness)
+    
+    def _get_voxel_spacing(self):
+        row, col = self.pixel_scaling
+        z = self.slice_thickness
+        return (z, row, col)
+    
+    def _build_volume(self):
+        return convert_to_hu(self.slices)
+    
+    def get_slice(self, idx):
+        img = self.volume[idx]
+        img = window_image(img)
+        return img
+    
+    def __len__(self):
+        return self.volume.shape[0]
