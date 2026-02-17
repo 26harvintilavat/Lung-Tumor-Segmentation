d0208e98ee07583e1a58473a621265551e6066ee
26harvintilavat <tilavatharvin@gmail.com>
2026-02-17 16:14:16 +0530

    Add validation Dice evaluation script

---
+++ scripts/evaluate.py
@@ -0,0 +1,58 @@
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(str(PROJECT_ROOT))
+
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+
+from configs.config import RAW_DATA_DIR, MASK_DIR, BATCH_SIZE, VAL_SPLIT, SEED
+from src.train_dataset import LungSegmentationDataset
+from src.model import UNet
+from scripts.prepare_dataloaders import get_patient_ids, split_patients
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def dice_score(pred, target):
+    pred = (pred > 0.5).float()
+    intersection = (pred * target).sum()
+    return (2. * intersection) / (pred.sum() + target.sum() + 1e-6)
+
+def main():
+    patient_ids = get_patient_ids(MASK_DIR)
+    _, val_ids = split_patients(patient_ids, VAL_SPLIT, SEED)
+
+    val_dataset = LungSegmentationDataset(RAW_DATA_DIR, MASK_DIR, val_ids)
+
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+    model = UNet(in_channels=1, out_channels=1).to(device)
+
+    checkpoint_path = Path("checkpoints/best_model.pth")
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+
+    if isinstance(checkpoint, dict):
+        model.load_state_dict(checkpoint['model_state_dict'])
+    else:
+        model.load_state_dict(checkpoint)
+
+    model.eval()
+
+    dices = []
+    with torch.no_grad():
+        for images, masks in val_loader:
+            images = images.to(device)
+            masks = masks.to(device)
+
+            outputs = model(images)
+            preds = torch.sigmoid(outputs)
+
+            for p, m in zip(preds, masks):
+                dices.append(dice_score(p, m).item())
+
+    print(f"\nFinal Validation Dice: {np.mean(dices):.4f}")
+
+if __name__=="__main__":
+    main()
